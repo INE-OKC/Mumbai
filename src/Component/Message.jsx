ebc97f86f058d9f6937647147fb1f1a561f9ebc2
Ciscoflash <abrahamjude1999@gmail.com>
2023-05-31 20:26:39 +0100

    message route

--- src/Component/Message.jsx
+++ src/Component/Message.jsx
@@ -7,6 +7,7 @@ import profile from "../assets/image/ineProfile.png";
 import savings from "../assets/image/ineSaving.png";
 import bell from "../assets/image/ineBell.png";
 import { BiDownArrow } from "react-icons/bi";
+import { Link } from "react-router-dom";
 const Message = () => {
   return (
     <div className="message">
@@ -19,19 +20,19 @@ const Message = () => {
           <div className="exp">
             <div>
               <img src={Home} alt="" />
-              <p>Home</p>
+              <Link to="/">Home</Link>
             </div>
             <div>
               <img src={explore} alt="" />
-              <p>Explore</p>
+              <Link to="/explore">Explore</Link>
             </div>
             <div>
               <img src={message} alt="" />
-              <p>Message</p>
+              <Link to="/message">Message</Link>
             </div>
             <div>
               <img src={profile} alt="" />
-              <p>Profile</p>
+              <Link to="/profile">Profile</Link>
             </div>
             <div>
               <img src={savings} alt="" />
@@ -99,7 +100,7 @@ const Message = () => {
         </div>
         <div className="third">
           <div className="thirdTop">
-          <div className="imgMessage">
+            <div className="imgMessage">
               <div className="Active"></div>
             </div>
             <div>
@@ -108,9 +109,36 @@ const Message = () => {
             </div>
             <div className="borders"></div>
           </div>
-          <div className="chats">
-          <div className="imgMessage">
-              <div className="Active"></div>
+          <div className="mainChat">
+            <div className="chats">
+              <div className="imgMessage">
+                <div className="Active"></div>
+              </div>
+              <div className="texts">
+                <div>
+                  <p>Ope</p> <i>42 min ago</i>
+                </div>
+                <span>
+                  Hey there! I see you're interested in Solana, too. What got
+                  you into the platform?
+                </span>
+                <span>Call me if you get this okay.</span>
+              </div>
+            </div>
+            <div className="chats chat1">
+              <div className="texts">
+                <div>
+                  <p>Me</p> <i>12 min ago</i>
+                </div>
+                <span>
+                  Hi! Yeah, I've been following Solana for a while now. I was
+                  really impressed with the platform's speed and scalability,
+                  and I believe it has a lot of potential for the future.
+                </span>
+              </div>
+              <div className="imgMessage">
+                <div className="Active"></div>
+              </div>
             </div>
           </div>
         </div>
