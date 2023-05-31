2e4fde055b1df8fddc4129cfe1eb77c02f389c95
Anthonyslav1 <anthony.nweke2002@gmail.com>
2023-05-31 22:33:39 +0100

    OK

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const APP_CONSTANTS = {
+    ALCHEMY_KEY:
+      "https://www.oklink.com/okexchain-test",
+    CONTRACT_ADDRESS: "0x8209Ce8F61Ee19ef5CF47fB4f982944b312C9bC8",
+    OPENSEA_ASSETS_URL: "https://testnets.opensea.io/assets/",
+    CLIENT_ID:
+      "BGhy-DvEdndjNw_qcxryh6CqOfm6W9mk_IQtAxzHHy0cNguHCG3F2FODub400gtPR-bLkPsx-1uf7MRZqBE12kw",
+    RPC_TARGET:
+      "https://www.oklink.com/okexchain-test",
+
+    REACT_APP_REFRESH_TIMER: 30,
+  };
