9f59fb1156b3ade9a95de5df1b7060242adc1da7
Anthony Nweke <75612396+Anthonyslav1@users.noreply.github.com>
2023-06-22 10:00:11 +0100

    Update Footer.jsx

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -10,24 +10,34 @@ function Footer() {
           &copy; 2023 INE. All rights reserved.
         </p>
       </div>
-      <div className="text-white ">
+     <div className="text-white ">
         <h3 className="font-semibold text-lg pb-3 md:text-xl">INE</h3>
-        <p className="text-[0.6rem] pb-3 md:text-sm">
-          <a href="/">About INE </a>
+       <p className="text-[0.6rem] pb-3 md:text-sm">
+          <Link href="https://https://pitch.com/public/2373a3ad-d6c3-4683-ad7f-aee85ad6c3d8">
+          <a className="underline underline-offset-1">Pitchdeck</a>
+        </Link>
         </p>
         <p className="text-[0.6rem] pb-3 md:text-sm">
-          <a href="/">Web App</a>
+          <Link href="https://www.twitter.com/anthonyslav1">
+          <a className="underline underline-offset-1">Twitter</a>
+        </Link>
         </p>
         <p className="text-[0.6rem] pb-3 md:text-sm">
-          <a href="/">Chrome Extension</a>
+          <Link href="https://github.com/INE-OKC/Mumbai/tree/Unity">
+          <a className="underline underline-offset-1">Github</a>
+        </Link>
+        </p>
+           <p className="text-[0.6rem] pb-3 md:text-sm">
+          <Link href="https://www.linkedin.com/in/anthonyslav1">
+          <a className="underline underline-offset-1">Meet the Project lead</a>
+        </Link>
         </p>
         <p className="text-[0.6rem] pb-3 md:text-sm">
-          <a href="/">Whisk for Business</a>
+          <Link href="https://wa.me/2348160276480?text=I'm%20interested%20in%20joining%20your%20INE%20team">
+          <a className="underline underline-offset-1">Join our dev team</a>
+        </Link>
         </p>
-        <p className="text-[0.6rem] pb-3 md:text-sm">
-          <a href="/">Careers</a>
-        </p>
-      </div>
+       </div>
       <div className="text-white">
         <h3 className="font-semibold text-lg pb-3 md:text-xl">Resources</h3>
         <p className="text-[0.6rem] pb-3 md:text-sm">
