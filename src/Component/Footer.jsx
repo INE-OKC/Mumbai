2679d55fc46fe209dd11c730e28cf354458e4aba
Anthony Nweke <75612396+Anthonyslav1@users.noreply.github.com>
2023-06-22 10:51:09 +0100

    Update Footer.jsx

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,4 +1,5 @@
-  import footerlogo from '../assets/image/INE.png'
+  import footerlogo from '../assets/image/INE.png';
+  import Link from 'next/link';
 
 function Footer() {
   return (
@@ -12,6 +13,26 @@ function Footer() {
       </div>
       <div className="text-white ">
         <h3 className="font-semibold text-lg pb-3 md:text-xl">INE</h3>
+        <Link href="https://pitch.com/public/2373a3ad-d6c3-4683-ad7f-aee85ad6c3d8">
+  <a className="underline underline-offset-1">Pitchdeck</a>
+</Link>
+
+<Link href="https://www.twitter.com/anthonyslav1">
+  <a className="underline underline-offset-1">Twitter</a>
+</Link>
+
+<Link href="https://github.com/INE-OKC/Mumbai/tree/Unity">
+  <a className="underline underline-offset-1">Github</a>
+</Link>
+
+<Link href="https://www.linkedin.com/in/anthonyslav1">
+  <a className="underline underline-offset-1">Meet the Project Lead</a>
+</Link>
+
+<Link href="https://wa.me/2348160276480?text=I'm%20interested%20in%20joining%20your%20INE%20team">
+  <a className="underline underline-offset-1">Join our dev team</a>
+</Link>
+
         <p className="text-[0.6rem] pb-3 md:text-sm">
           <a href="/">About INE </a>
         </p>
