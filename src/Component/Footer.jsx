1e5b689c433c9c1edbb2f325f02152e87354389a
Anthony Nweke <75612396+Anthonyslav1@users.noreply.github.com>
2023-06-22 09:53:12 +0100

    Update Footer.jsx

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -30,18 +30,16 @@ function Footer() {
         </p>
       </div>
       <div className="text-white">
-        <h3 className="font-semibold text-lg pb-3 md:text-xl">Resources</h3>
+        <h3 className="font-semibold text-lg pb-3 md:text-xl">INE Dapp</h3>
+        
         <p className="text-[0.6rem] pb-3 md:text-sm">
-          <a href="/">Support</a>
+          <Link to="Dapp">Launch Dapp</Link>
         </p>
         <p className="text-[0.6rem] pb-3 md:text-sm">
-          <a href="/">Blog</a>
+         <Link to="safe">Save with INE</Link>
         </p>
         <p className="text-[0.6rem] pb-3 md:text-sm">
-          <a href="/">Partners</a>
-        </p>
-        <p className="text-[0.6rem] pb-3 md:text-sm">
-          <a href="/">Press</a>
+          <Link to="Inenft">Become a Pro Member</Link>
         </p>
       </div>
     </footer>
